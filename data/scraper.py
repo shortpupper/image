8858e590438e969916c5c2463ab39bc5b94afa28
lole <loganboganfarfignogan@gmail.com>
2022-04-29 16:36:50 -0500

    yes

---
+++ data/scraper.py
@@ -0,0 +1,14 @@
+# import requests
+
+# url = "https://www.thewordfinder.com/random-sentence-generator/"
+
+# # then request that url
+# request = requests.get(url)
+
+# # then print the text
+
+# print(request.text)
+
+# import the import to control the keyborad
+import pyautogui
+

b4f96b6e66fcddc4e8ff5a6f207a3ba72e2c6069
PythonGithCode <99050787+PythonGithCode@users.noreply.github.com>
2022-05-05 08:41:50 -0500

    Update mainf.py

--- mainf.py
+++ mainf.py
@@ -98,4 +98,4 @@ def doimage(text, name):
 h = "{}".format(readimage(os.getcwd()+"/mon.png"))[2:-1]
 print(type(h), h.replace("\\", "\ "[:-1]))
 
-
+#https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script
